3ef3e5402bed3066be175302e858e9782c000861
okeokke <okeokke77@gmail.com>
2025-11-11 21:45:15 +0700

    Hotfix SearchNilai08.java. Teledor salah ketik di bagian perulangan dalam loop. (hasil = 1 instead of i)

--- SearchNilai08.java
+++ SearchNilai08.java
@@ -5,15 +5,16 @@ public class SearchNilai08 {
     int arrNilai[] = {80, 85, 78, 96, 90, 82, 86};
     int key = 90;
     int hasil = 0;
+
     for (int i = 0; i < arrNilai.length; i++) {
         if (key == arrNilai [i]){
-          hasil = 1;
+          hasil = i;
           break;
         }
     }
     System.out.println();
-    System.out.println();
     System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+    System.out.println();
     sc.close();
   }
 }
